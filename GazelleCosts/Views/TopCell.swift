fc46ce1a3ff391a6213a3028ce90dd6b60d3197f
Efimenko Aleksandr <gazservissana.zp@gmail.com>
2019-01-02 12:00:21 +0200

    create background

---
+++ GazelleCosts/Views/TopCell.swift
@@ -0,0 +1,24 @@
+//
+//  TopCell.swift
+//  GazelleCosts
+//
+//  Created by user on 12/28/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class TopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
